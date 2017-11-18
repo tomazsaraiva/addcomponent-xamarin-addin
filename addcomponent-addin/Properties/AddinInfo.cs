fc6f137ec2362330b8d66cad36c2d231e36a4a6e
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2017-11-18 21:39:00 +0000

    initial commit with two new file templates

---
+++ addcomponent-addin/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "AddComponent Extensions",
+    Namespace = "addcomponent.extensions",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("Add Component Extensions")]
+[assembly: AddinCategory("IDE Extensions")]
+[assembly: AddinDescription("A bunch of new file templates to make your life a lot easier")]
+[assembly: AddinAuthor("Tomaz Saraiva")]
+
+[assembly: AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
